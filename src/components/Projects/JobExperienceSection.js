ce3ac15e3d00a67c1991557c997a70f6d1271f00
ericmah123 <emah2@ualberta.ca>
2025-05-11 00:35:54 -0600

    Fixed deployment

--- src/components/Projects/JobExperienceSection.js
+++ src/components/Projects/JobExperienceSection.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import React, { useState } from "react";
 import { Container } from "react-bootstrap";
 import { motion, AnimatePresence } from "framer-motion";
 import { useInView } from "react-intersection-observer";
